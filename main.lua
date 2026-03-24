31810c4ccc559cda7d2faeee1fcb5ecdd6b509e4
FlashScripts0 <jesterxdlmao@gmail.com>
2026-03-24 23:15:48 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6Ijg1MTk3YTU0LTQ0ODAtNDI0Yy05Yzg2LTkwOWVhODczNzQ3NSIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6Ijg1MTk3YTU0LTQ0ODAtNDI0Yy05Yzg2LTkwOWVhODczNzQ3NSIsImtpbmQiOiJsb2FkZXIifQ"))()
